71257c374f78c24dd973dcef2e23284c672ebeef
Roman <amorka89@gmail.com>
2016-08-16 21:03:58 +0300

    s

---
+++ MailBomber/Mails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+
+namespace MailBomber
+{
+    class Mails
+    {
+        [PrimaryKey, AutoIncrement, Unique]
+        public int id { get; set; }
+        [NotNull]
+        public string mail { get; set; }
+    }
+}
