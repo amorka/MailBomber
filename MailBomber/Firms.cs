190ff2ed82f6846cde4274f6179e9018cdc26335
Roman <amorka89@gmail.com>
2016-08-16 21:08:05 +0300

    s

---
+++ MailBomber/Firms.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+namespace MailBomber
+{
+    class Firms
+    {
+        [PrimaryKey, AutoIncrement, Unique]
+        public int id { get; set; }
+        [NotNull]
+        public string name { get; set; }
+    }
+}
